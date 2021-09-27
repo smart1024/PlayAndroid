dda94111a6be9c651794a007df1d0ef6c8429f49
liling <1059232202@qq.com>
2021-09-27 10:47:01 +0800

    Kotlin实现工具方法实现求Comparable<T>子类大小比较

--- Kotlin_CustomUtil/app/src/main/java/com/lilin/android/kotlin_customutil/LearnKotlin.kt
+++ Kotlin_CustomUtil/app/src/main/java/com/lilin/android/kotlin_customutil/LearnKotlin.kt
@@ -1,5 +1,6 @@
 package com.lilin.android.kotlin_customutil
 
+import java.lang.RuntimeException
 import kotlin.jvm.internal.Intrinsics
 
 /**
@@ -9,15 +10,37 @@ import kotlin.jvm.internal.Intrinsics
  * 包名： com.lilin.android.kotlin_customutil
  * 类说明：
  */
+
+/**
+ * 编写好用的工具方法
+ */
 fun main(){
     val max = max(20, 10, 90)
     println(max)
+    val max1 = max(1.0,2.0,7.0)
+    println(max1)
+    val max2 = max(1L,2L,3L)
+    println(max2)
 }
 
 fun max(vararg nums:Int):Int{
     var max = Int.MIN_VALUE
     for (i in nums){
         max = kotlin.math.max(max,i)
+    }
+    return max
+}
+
+/**
+ * Comparable<T>的子类都可以，使用泛型支持能多类型数据比较
+ */
+fun <T:Comparable<T>> max(vararg nums: T):T{
+    if (nums.isEmpty()) throw RuntimeException("params can not be empty")
+    var max = nums[0]
+    for (num in nums){
+        if (num > max){
+            max = num
+        }
     }
     return max
 }
---
+++ Kotlin_CustomUtil/app/src/main/java/com/lilin/android/kotlin_customutil/Test.java
@@ -0,0 +1,14 @@
+package com.lilin.android.kotlin_customutil;
+
+/**
+ * 创建日期：2021/9/27 10:32
+ *
+ * @author lilin
+ * @version 1.0
+ * 包名： com.lilin.android.kotlin_customutil
+ * 类说明：
+ */
+
+public class Test {
+
+}
