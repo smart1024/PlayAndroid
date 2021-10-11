c50c5b00c2e2e01cd832de8e8d29c809fe360548
liling <1059232202@qq.com>
2021-10-11 17:45:33 +0800

    lottie动画加载AE导出的json文件

--- TodayInformation/app/src/main/java/com/lilin/android/todayinformation/MainActivity.kt
+++ TodayInformation/app/src/main/java/com/lilin/android/todayinformation/MainActivity.kt
@@ -2,10 +2,25 @@ package com.lilin.android.todayinformation
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.LayoutInflater
+import com.lilin.android.todayinformation.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        val inflate = ActivityMainBinding.inflate(LayoutInflater.from(this))
+        setContentView(inflate.root)
+
+        inflate.lav1.playAnimation()
+
+        inflate.lav1.setOnClickListener {
+            inflate.lav1.playAnimation()
+            inflate.lav2.reverseAnimationSpeed()
+        }
+
+        inflate.lav2.setOnClickListener {
+            inflate.lav2.playAnimation()
+            inflate.lav1.reverseAnimationSpeed()
+        }
     }
 }
