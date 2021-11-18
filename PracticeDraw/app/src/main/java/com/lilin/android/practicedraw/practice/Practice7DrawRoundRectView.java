ef01aa12c54c6f4d88b6d599b9f5cd3a2b060a2b
liling <1059232202@qq.com>
2021-11-18 17:41:48 +0800

    drawRoundRect

--- PracticeDraw/app/src/main/java/com/lilin/android/practicedraw/practice/Practice7DrawRoundRectView.java
+++ PracticeDraw/app/src/main/java/com/lilin/android/practicedraw/practice/Practice7DrawRoundRectView.java
@@ -2,12 +2,14 @@ package com.lilin.android.practicedraw.practice;
 
 import android.content.Context;
 import android.graphics.Canvas;
-import androidx.annotation.Nullable;
+import android.graphics.Paint;
 import android.util.AttributeSet;
 import android.view.View;
 
-public class Practice7DrawRoundRectView extends View {
+import androidx.annotation.Nullable;
 
+public class Practice7DrawRoundRectView extends View {
+    private Paint paint = new Paint();
     public Practice7DrawRoundRectView(Context context) {
         super(context);
     }
@@ -25,5 +27,13 @@ public class Practice7DrawRoundRectView extends View {
         super.onDraw(canvas);
 
 //        练习内容：使用 canvas.drawRoundRect() 方法画圆角矩形
+        //rx ry分别是圆角横向半径、和纵向半径
+        canvas.drawRoundRect(100, 100,500,300, 50, 50,paint);
+
+
+        paint.setStyle(Paint.Style.STROKE);
+        paint.setStrokeWidth(10);
+        canvas.drawRoundRect(100, 400,500,600, 50, 50,paint);
+
     }
 }
