8921b790de690ee06ea8bc09b36b8fb7f2cc7cdc
liling <1059232202@qq.com>
2021-11-18 17:56:45 +0800

    绘制连接圆心、不连圆心圆弧，圆弧线

--- PracticeDraw/app/src/main/java/com/lilin/android/practicedraw/practice/Practice8DrawArcView.java
+++ PracticeDraw/app/src/main/java/com/lilin/android/practicedraw/practice/Practice8DrawArcView.java
@@ -2,13 +2,14 @@ package com.lilin.android.practicedraw.practice;
 
 import android.content.Context;
 import android.graphics.Canvas;
+import android.graphics.Paint;
 import android.util.AttributeSet;
 import android.view.View;
 
 import androidx.annotation.Nullable;
 
 public class Practice8DrawArcView extends View {
-
+    private Paint paint = new Paint();
     public Practice8DrawArcView(Context context) {
         super(context);
     }
@@ -26,5 +27,22 @@ public class Practice8DrawArcView extends View {
         super.onDraw(canvas);
 
 //        练习内容：使用 canvas.drawArc() 方法画弧形和扇形
+
+        /**
+         * left,top,right,bottom 表示圆弧所在矩形
+         * useCenter表示是否连接到圆心
+         * startAngle:起始角度，顺时针方向与x轴夹角为正，反之为负
+         * sweepAngle:扫过的角度
+         */
+//        canvas.drawArc(200, 100, 800, 500,250);
+        paint.setAntiAlias(true);
+        //
+        canvas.drawArc(200, 100, 800, 500, 250, 100, true, paint); // 绘制扇形
+
+        //填充，不连接圆心
+        canvas.drawArc(200, 100, 800, 500, 20, 140, false, paint); // 绘制弧形
+
+        paint.setStyle(Paint.Style.STROKE);
+        canvas.drawArc(200, 100, 800, 500, 180, 60, false, paint); // 绘制不封口的弧形
     }
 }
