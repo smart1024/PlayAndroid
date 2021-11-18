3e3deba9cc03993582e1f47370201e037ef943d7
liling <1059232202@qq.com>
2021-11-18 16:03:46 +0800

    drawOval

--- PracticeDraw/app/src/main/java/com/lilin/android/practicedraw/practice/Practice5DrawOvalView.java
+++ PracticeDraw/app/src/main/java/com/lilin/android/practicedraw/practice/Practice5DrawOvalView.java
@@ -2,13 +2,16 @@ package com.lilin.android.practicedraw.practice;
 
 import android.content.Context;
 import android.graphics.Canvas;
+import android.graphics.Paint;
+import android.graphics.RectF;
 import android.util.AttributeSet;
+import android.util.DisplayMetrics;
 import android.view.View;
 
 import androidx.annotation.Nullable;
 
 public class Practice5DrawOvalView extends View {
-
+    private Paint paint = new Paint();
     public Practice5DrawOvalView(Context context) {
         super(context);
     }
@@ -26,5 +29,14 @@ public class Practice5DrawOvalView extends View {
         super.onDraw(canvas);
 
 //        练习内容：使用 canvas.drawOval() 方法画椭圆
+        //当宽高相等是等价于drawCircle
+        DisplayMetrics metrics = getResources().getDisplayMetrics();
+        float lw = 500;
+        int widthPixels = metrics.widthPixels;
+
+        float left = (widthPixels - lw)/ 2f;
+
+        RectF f = new RectF(left,150,left+lw,350);
+        canvas.drawOval(f,paint);
     }
 }
